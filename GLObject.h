d119da17660038a7b2663a24c9f74e491814f3a3
ap405405 <zyh910605@126.com>
2016-11-30 13:14:15 +0900

    ds
    dsd

--- GLObject.h
+++ GLObject.h
@@ -86,6 +86,7 @@ public:
 
 	void rotateCenteredZAxis(const float& angle_degree)
 	{
+		//model_matrix_ = glm::translate(...) * glm::rotate(...) * glm::translate(...) * model_matrix_;
 		model_matrix_ = glm::translate(center_) * glm::rotate(glm::mat4(), glm::radians(angle_degree), glm::vec3(0.0f, 0.0f, 1.0f)) * glm::translate(-center_) * model_matrix_;
 
 	}
